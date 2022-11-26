f097e2d2d07b30a4e53d1982f1c9749194eaafbd
matthieu <rei_eva@hotmail.com>
2022-11-26 17:21:30 +0100

    refactor quill fields, simplify code

---
+++ src/DTO/Fields/BlockField/AlignField.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DTO\Fields\BlockField;
+
+use App\DTO\Fields\Interfaces\QuillBlockFieldInterface;
+
+final class AlignField implements QuillBlockFieldInterface
+{
+    public const ALIGN_FIELD_OPTION_LEFT = false;
+    public const ALIGN_FIELD_OPTION_CENTER = 'center';
+    public const ALIGN_FIELD_OPTION_RIGHT = 'right';
+    public const ALIGN_FIELD_OPTION_JUSTIFY = 'justify';
+
+    private array $options;
+
+    public function __construct(string|bool ...$options)
+    {
+        $this->options = $options;
+    }
+
+    public function getOption(): array
+    {
+        $array = [];
+        $array['align'] = $this->options;
+
+        return $array;
+    }
+}
