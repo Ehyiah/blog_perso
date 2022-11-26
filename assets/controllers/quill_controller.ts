5c919a1f5b397ebd9f919aa06e104c1f12617b50
matthieu <rei_eva@hotmail.com>
2022-11-26 17:24:37 +0100

    ts update

--- assets/controllers/quill_controller.ts
+++ assets/controllers/quill_controller.ts
@@ -8,46 +8,36 @@ export default class extends Controller {
     static targets = ['babar'];
     static values = {
         toolbarOptions: {
-            // type: String,
-            // default: '',
-            // type: Object,
-            // default: {}
             type: Array,
             default: ['bold', 'italic', ['underline']]
         },
     }
 
-    // readonly inputTarget: HTMLInputElement;
-    // readonly placeholderTarget: HTMLDivElement;
-    // readonly previewClearButtonTarget: HTMLButtonElement;
-
     connect() {
-        console.log(this.toolbarOptionsValue)
-        // console.log(this.babarTarget);
-        // console.log(this.babarTarget.getAttribute('data-quill-target'));
-        // console.log(this.babarTarget.getAttributeNode('data-quill-target'));
-        // console.log(this.babarTarget.textContent);
         console.log('coucou from quill controller');
 
         const test = this.toolbarOptionsValue;
         const toolbarOptions = [
-            ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-            ['blockquote', 'code-block'],
+            // ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+            // ['blockquote', 'code-block'],
 
-            [{ 'header': 1 }, { 'header': 2 }],               // custom button values
-            [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-            [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-            [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-            [{ 'direction': 'rtl' }],                         // text direction
+            // [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+            // [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+            // [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+            // [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+            // [{ 'direction': 'rtl' }],                         // text direction
 
-            [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-            [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-            [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-            [{ 'font': [] }],
-            [{ 'align': [] }],
-
-            ['clean']                                         // remove formatting button
+            // [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+            // [{ 'header': [1, 2, 3, 4, 5, 6, false] }, { 'color': ['green', 'blue', 'red'] }],
+            // [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+            // [{ 'color': ['green', 'blue', 'red'] }],
+            //
+            // [{ 'color': ['green', 'blue', 'red'] }, { 'background': [] }],          // dropdown with defaults from theme
+            // [{ 'font': [] }],
+            // [{ 'align': [] }],
+            //
+            // ['link', 'image'],
+            // ['clean']                                         // remove formatting button
         ];
 
         console.log(test)
